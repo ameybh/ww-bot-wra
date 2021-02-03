10f9fdcee1ffb966d04f0636e08038f8aab9703f
Amey Bhavsar <iamamey24@gmail.com>
2021-02-03 12:34:34 +0530

    better invoke check

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ require('dotenv').config();
 const qrcode = require('qrcode-terminal');
 const { Client } = require('whatsapp-web.js');
 
+
+// setup Wolfram|Alpha API
+const appid = process.env.APPID;
+const WolframAlphaAPI = require('./lib/WolframAlphaAPI.js');
+let wraAPI = WolframAlphaAPI(appid);
+const invokeKey = 'wra';
+
 let sessionLocal = JSON.parse(process.env.WW_SESSION);
 console.log(sessionLocal);
 
@@ -23,16 +30,23 @@ client.on('authenticated', session => {
     // Save this session object in WW_SESSION manually to reuse it next time
     console.log(JSON.stringify(session));
 });
-
+client.on('auth_failure', msg => {
+    // Fired if session restore was unsuccessfull
+    console.error('AUTHENTICATION FAILURE', msg);
+});
 client.on('ready', () => {
     console.log('Client is ready!');
 });
 
 client.on('message', message => {
     console.log(message);
-    const check = message.body.toLowerCase();
-    if (check.indexOf('!hi') != -1 || check.indexOf('!hello') != -1) {
-        message.reply('Hello there!\nI am ww-bot. This is an automated message.\nRead more at https://github.com/ameybhavsar24/ww-bot');
+    // only reply if message contains invoke keyword as first word
+    // example: !wra <message here>
+    if (message.body.startsWith('!' + invokeKey + ' ')) {
+    	// send the rest of the message to Wolfram|Alpha API
+    	const actualMessage = message.body.substring(1 + invokeKey.length);
+    	wraAPI.getShort(actualMessage)
+    		.then(res => message.reply(res));
     }
 });
 
